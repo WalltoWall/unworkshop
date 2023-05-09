7a6ff8ca863a847c6951c3d8fb53824fcda6350c
Anthony Yarbrough <asyarb@hawaii.edu>
2023-05-08 16:47:01 -1000

    fix: register cleanup

--- app/(site)/kickoff/[code]/page.tsx
+++ app/(site)/kickoff/[code]/page.tsx
@@ -1,12 +1,35 @@
+import { cookies } from "next/headers"
+import { PARTICIPANT_COOKIE } from "@/constants"
 import type { Metadata } from "next"
+import { findKickoff, findParticipant } from "@/sanity/client"
+import { notFound, redirect } from "next/navigation"
 
 type Props = {
 	params: { code: string }
 	searchParams: { [key: string]: string | string[] | undefined }
 }
 
-const KickoffPage = (props: Props) => {
-	return <div>Kickoff</div>
+function redirectToRegister(code: string): never {
+	redirect("/kickoff/register?code=" + code)
+}
+
+const KickoffPage = async (props: Props) => {
+	const participantId = cookies().get(PARTICIPANT_COOKIE)?.value
+
+	if (!participantId) redirectToRegister(props.params.code)
+
+	const [participant, kickoff] = await Promise.all([
+		findParticipant(participantId),
+		findKickoff(props.params.code),
+	])
+
+	// If this participant was registered to a different Kickoff, redirect them
+	// to re-register too.
+	if (!participant || participant.kickoff._ref !== kickoff?._id)
+		redirectToRegister(props.params.code)
+	if (!kickoff) notFound()
+
+	return <div className="dynamic-screen bg-white text-black">Kickoff</div>
 }
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
