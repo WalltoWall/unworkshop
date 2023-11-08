d4d3f732e91d0c1607ea4dd9c6f5894df70d4fd1
keyranova <keyra@walltowall.com>
2023-11-08 11:22:47 -0500

    creates quadrant image component

--- app/(site)/kickoff/[code]/exercises/[slug]/Quadrant.tsx
+++ app/(site)/kickoff/[code]/exercises/[slug]/Quadrant.tsx
@@ -65,6 +65,16 @@ export interface DragItem {
 	left: number
 }
 
+const QuadrantImage = ({ image }) => (
+	<Image
+		src={urlFor(image).width(150).height(150).format("webp").toString()}
+		alt={altFor(image)}
+		className="max-h-[45px] w-full max-w-[45px] object-contain sm:max-h-[90px] sm:max-w-[90px]"
+		width={150}
+		height={150}
+	/>
+)
+
 type QuadrantProps = {
 	item: NonNullable<ST["exercise"]["quadrants"]>[number]
 	index: number
@@ -235,17 +245,17 @@ export const Quadrant = ({
 				</div>
 			)}
 
-			<div className="relative p-12">
-				<h3 className="absolute left-0 top-5 w-full text-center text-gray-50">
+			<div className="relative p-7 md:p-12">
+				<h3 className="absolute left-0 top-2 w-full text-center text-gray-50 md:top-5">
 					{item.topValue}
 				</h3>
-				<h3 className="absolute bottom-5 left-0 w-full text-center text-gray-50">
+				<h3 className="absolute bottom-1 left-0 w-full text-center text-gray-50 md:bottom-5">
 					{item.bottomValue}
 				</h3>
-				<h3 className="absolute bottom-0 left-11 w-full origin-bottom-left -rotate-90 text-center text-gray-50">
+				<h3 className="absolute bottom-0 left-7 w-full origin-bottom-left -rotate-90 text-center text-gray-50 md:left-11">
 					{item.leftValue}
 				</h3>
-				<h3 className="absolute bottom-0 right-11 w-full origin-bottom-right rotate-90 text-center text-gray-50">
+				<h3 className="absolute bottom-0 right-7 w-full origin-bottom-right rotate-90 text-center text-gray-50 md:right-11">
 					{item.rightValue}
 				</h3>
 
@@ -255,62 +265,22 @@ export const Quadrant = ({
 				>
 					<div className="absolute left-0 top-0 flex h-1/2 w-1/2 items-center justify-center border-b-2 border-r-2 border-gray-50">
 						{item.topLeftImage?.asset && (
-							<Image
-								src={urlFor(item.topLeftImage)
-									.width(150)
-									.height(150)
-									.format("webp")
-									.toString()}
-								alt={altFor(item.topLeftImage)}
-								className="max-h-[90px] w-full max-w-[90px] object-contain"
-								width={150}
-								height={150}
-							/>
+							<QuadrantImage image={item.topLeftImage} />
 						)}
 					</div>
 					<div className="absolute right-0 top-0 flex h-1/2 w-1/2 items-center justify-center border-b-2 border-gray-50">
 						{item.topRightImage?.asset && (
-							<Image
-								src={urlFor(item.topRightImage)
-									.width(150)
-									.height(150)
-									.format("webp")
-									.toString()}
-								alt={altFor(item.topRightImage)}
-								className="max-h-[90px] w-full max-w-[90px] object-contain"
-								width={150}
-								height={150}
-							/>
+							<QuadrantImage image={item.topRightImage} />
 						)}
 					</div>
 					<div className="absolute bottom-0 left-0 flex h-1/2 w-1/2 items-center justify-center border-r-2 border-gray-50">
 						{item.bottomLeftImage?.asset && (
-							<Image
-								src={urlFor(item.bottomLeftImage)
-									.width(150)
-									.height(150)
-									.format("webp")
-									.toString()}
-								alt={altFor(item.bottomLeftImage)}
-								className="max-h-[90px] w-full max-w-[90px] object-contain"
-								width={150}
-								height={150}
-							/>
+							<QuadrantImage image={item.bottomLeftImage} />
 						)}
 					</div>
 					<div className="absolute bottom-0 right-0 flex h-1/2 w-1/2 items-center justify-center">
 						{item.bottomRightImage?.asset && (
-							<Image
-								src={urlFor(item.bottomRightImage)
-									.width(150)
-									.height(150)
-									.format("webp")
-									.toString()}
-								alt={altFor(item.bottomRightImage)}
-								className="max-h-[90px] w-full max-w-[90px] object-contain"
-								width={150}
-								height={150}
-							/>
+							<QuadrantImage image={item.bottomRightImage} />
 						)}
 					</div>
 
