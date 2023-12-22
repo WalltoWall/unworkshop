ad7d81c2494103de5fa053aa5f6dda8842e76c04
Anthony Yarbrough <asyarb@hawaii.edu>
2023-12-22 09:43:36 -1000

    review: quadrant steps

--- app/(site)/kickoff/[code]/exercises/[slug]/_QuadrantsExercise/QuadrantSteps.tsx
+++ app/(site)/kickoff/[code]/exercises/[slug]/_QuadrantsExercise/QuadrantSteps.tsx
@@ -7,6 +7,9 @@ import type { ST } from "@/sanity/config"
 import { Quadrant } from "./_Quadrant/Quadrant"
 import type { Answer } from "./types"
 
+// REVIEW: Instead of exporting this and re-calculating it, I think it would be
+// better if <QuadrantSteps /> derived it once and passed the result to the
+// other components that need it.
 export const getTime = (active: number, index: number) => {
 	if (active === index * 2) {
 		return "today"
@@ -14,6 +17,9 @@ export const getTime = (active: number, index: number) => {
 		return "tomorrow"
 	}
 
+	// REVIEW: Instead of returning false here, I think it would be better to
+	// encode the "idle" state as another literal. That way "getTime()" would
+	// always return some result like "idle" | "tomorrow" | "today".
 	return false
 }
 
@@ -39,8 +45,25 @@ export const QuadrantSteps = ({
 	kickoffCode,
 }: QuadrantStepsProps) => {
 	const router = useRouter()
+
+	// REVIEW: I think it may be better to keep track of the current step with a
+	// search parameter instead of React state, mostly to allow folks to
+	// preserve where they are in-case they close the tab or refresh
+	// on-accident.
 	const [active, setActive] = React.useState(0)
 
+	// REVIEW: Overall, the way that we're tracking the state of this exercise
+	// is a little hard to follow. Instead of trying to derive what state we're
+	// in based on what `active` is and the `answers` array, I think explicitly
+	// tracking the state with a string literal would really clear-up
+	// what's going on and when certain elements need to be disabled.
+	//
+	// For example, if we model the state of the exercise with just string literals:
+	//
+	// type State = "today_pending" | "today_placed" | "tomorrow_pending" | "tomorrow_placed" | "complete"
+	//
+	// From that, we can know that `disabled` is true whenever `state` is
+	// "today_pending" or "tomorrow_pending".
 	const handleDisabled = () => {
 		const tomorrow = (active / 2) % 1 > 0 ? true : false
 
@@ -83,6 +106,11 @@ export const QuadrantSteps = ({
 				))}
 			</div>
 
+			{/* REVIEW: The steps component seems to be pretty involved and needs to
+            know a lot about an exercise in order to function. Could <Steps />
+            maybe just be a dumber component that just updated a search
+            parameter and the exercise itself could keep track of what the
+            search parameter is? */}
 			<Steps
 				disabled={handleDisabled()}
 				count={quadrants.length * 2}
