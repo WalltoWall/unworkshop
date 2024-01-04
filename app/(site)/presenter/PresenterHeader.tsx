0c4b53b48a30c2b566361fdfe6f98001fa96a96a
keyranova <keyra@walltowall.com>
2024-01-04 13:44:32 -0500

    completes url param for presenter view

--- app/(site)/presenter/PresenterHeader.tsx
+++ app/(site)/presenter/PresenterHeader.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { useState } from "react"
+import { useState, type ChangeEvent } from "react"
 import Link from "next/link"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { cx } from "class-variance-authority"
+import { Chevron } from "@/components/icons/Chevron"
 import { Hamburger } from "@/components/icons/Hamburger"
 import { Logo } from "@/components/Logo"
 import { Text } from "@/components/Text"
 import type { ST } from "@/sanity/config"
+import { QuadrantsHeaderNav } from "./[code]/[slug]/_QuadrantsExercise/QuadrantsHeaderNav"
 
 interface PresenterHeaderProp {
 	kickoffCode?: string
@@ -21,7 +24,24 @@ export const PresenterHeader = ({
 	activeExercise,
 	heading,
 }: PresenterHeaderProp) => {
+	const router = useRouter()
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+	const step = parseInt(searchParams?.get("step") ?? "1")
+
+	const steps = activeExercise?.quadrants
+		? activeExercise.quadrants.length
+		: null
+
 	const [isOpen, setIsOpen] = useState(false)
+
+	const changeStep = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const params = new URLSearchParams({
+			step: event.target.value.toString(),
+		})
+
+		router.push(pathname + "?" + params.toString(), { scroll: false })
+	}
 
 	return (
 		<header className="relative z-50 flex h-[5.5rem] items-center bg-black px-8 py-[1.125rem]">
@@ -36,6 +56,24 @@ export const PresenterHeader = ({
 				>
 					{heading}
 				</Text>
+			)}
+
+			{steps && (
+				<div className="relative mx-6">
+					<select
+						value={step}
+						onChange={changeStep}
+						className="h-9 min-w-28 appearance-none rounded-lg border border-gray-58 bg-black pl-2 pr-7 pt-1 uppercase text-white text-18 font-heading"
+					>
+						{[...Array(steps)].map((_, index) => (
+							<option key={index} value={index + 1}>
+								Step {index + 1}
+							</option>
+						))}
+					</select>
+
+					<Chevron className="absolute right-2 top-3 h-[0.75rem] -rotate-90 text-white" />
+				</div>
 			)}
 
 			{exercises && (
@@ -62,6 +100,10 @@ export const PresenterHeader = ({
 									>
 										{exercise.name}
 									</Link>
+
+									{exercise.type === "quadrants" && (
+										<QuadrantsHeaderNav exercise={exercise} />
+									)}
 								</li>
 							))}
 						</ul>
@@ -71,3 +113,18 @@ export const PresenterHeader = ({
 		</header>
 	)
 }
+
+type StepNavItemProps = React.ComponentPropsWithoutRef<"button"> & {
+	active: boolean
+}
+
+export const StepNavItem = ({ active = false, ...props }: StepNavItemProps) => (
+	<button
+		type="button"
+		className={cx(
+			"mx-6 uppercase transition-opacity text-24 font-heading hover:opacity-100 focus:opacity-100",
+			active ? "opacity-100" : "opacity-50",
+		)}
+		{...props}
+	/>
+)
