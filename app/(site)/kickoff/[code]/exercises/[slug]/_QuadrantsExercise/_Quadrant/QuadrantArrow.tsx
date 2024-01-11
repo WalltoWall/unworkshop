e25299bbb9de613eba1511f4a699c5a7aa90282d
keyranova <keyra@walltowall.com>
2024-01-11 13:30:28 -0500

    adds arrow to all participant quadrant steps

--- app/(site)/kickoff/[code]/exercises/[slug]/_QuadrantsExercise/_Quadrant/QuadrantArrow.tsx
+++ app/(site)/kickoff/[code]/exercises/[slug]/_QuadrantsExercise/_Quadrant/QuadrantArrow.tsx
@@ -1,12 +1,10 @@
 import { cx } from "class-variance-authority"
-import type { State } from "../QuadrantSteps"
 import styles from "./QuadrantExercise.module.css"
 
 interface QuadrantArrowProps {
 	top?: number
 	left?: number
 	tomorrowPlaced: boolean
-	state: State
 	width: number
 	angle: number
 }
@@ -15,7 +13,6 @@ export const QuadrantArrow = ({
 	top,
 	left,
 	tomorrowPlaced,
-	state,
 	width,
 	angle,
 }: QuadrantArrowProps) => {
@@ -29,13 +26,8 @@ export const QuadrantArrow = ({
 				transform: `rotate(${angle}deg)`,
 			}}
 		>
-			<div className={cx("bg-indigo-68", state === "complete" && "!w-full")} />
-			<div
-				className={cx(
-					"border-l-indigo-68",
-					state === "complete" && "opacity-0",
-				)}
-			/>
+			<div className="bg-indigo-68" />
+			<div className="border-l-indigo-68" />
 		</div>
 	) : null
 }
