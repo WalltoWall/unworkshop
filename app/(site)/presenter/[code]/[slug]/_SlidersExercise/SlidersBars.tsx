97ee6daf46a74653a8bb4052e92cd11d45abd901
David Johnson <davidj.media@gmail.com>
2024-01-29 13:54:46 -0500

    updates bars to be a percentage of the answers. This accomdates for groups where answers may go above 10.

--- app/(site)/presenter/[code]/[slug]/_SlidersExercise/SlidersBars.tsx
+++ app/(site)/presenter/[code]/[slug]/_SlidersExercise/SlidersBars.tsx
@@ -25,11 +25,17 @@ export const SlidersBars = ({
 		for (let i = 1; i <= 6; i++) {
 			let countToday = 0
 			let countTomorrow = 0
+
 			// find answers that match iteration 
             answers[slider.slug.current].map((answer: any) => (
                 (answer.today == i) ? countToday++ : countToday,
                 (answer.tomorrow == i) ? countTomorrow++ : countTomorrow
             ))
+            
+            let allAnswers = countToday + countTomorrow
+            let todayResults = countToday/allAnswers * 100
+            let tomorrowResults = countTomorrow/allAnswers * 100
+
 			
 			// push html to the array
 			content.push(
@@ -37,14 +43,14 @@ export const SlidersBars = ({
                     <div 
                         className={"relative block w-full max-w-[4rem] bg-yellow-68"}
                         style={{
-                            height: countToday == 0 ? "1rem" : `${countToday * 10}%`,
+                            height: countToday == 0 ? "1rem" : `${todayResults}%`,
                         }}
                     ></div>
                     <div 
                         key={i}
                         className={"relative block w-full max-w-[4rem] bg-yellow-68"}
                         style={{
-                            height: countTomorrow == 0 ? "1rem" : `${countTomorrow * 10}%`,
+                            height: countTomorrow == 0 ? "1rem" : `${tomorrowResults}%`,
                             backgroundImage: `repeating-linear-gradient(-45deg,${color},${color} 2px,rgba(0,0,0,0) 2px,rgba(0,0,0,0) 18px)`,
                             backgroundColor: 'transparent',
                             border: `2px solid ${color}`,
@@ -58,7 +64,7 @@ export const SlidersBars = ({
 	}
 
 	return (
-		<div className="top-0 left-0 right-0 px-4 w-full flex justify-evenly items-end gap-4 lg:gap-8"
+		<div className="top-0 left-0 right-0 p-4 w-full flex justify-evenly items-end gap-4 lg:gap-8"
         style={{
             position: images ? 'absolute' : 'static',
             height: images ? 'calc(50vh - 0.666rem)' : '100%'
