343d35d8e3a0a933c79ff0a30153df7aa43d7f64
Anthony Yarbrough <asyarb@hawaii.edu>
2024-04-24 11:13:35 -1000

    add back in build qa checks

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
 	reactStrictMode: false,
 	logging: { fetches: { fullUrl: true } },
-	typescript: { ignoreBuildErrors: true },
-	eslint: { ignoreDuringBuilds: true },
 	images: {
 		formats: ["image/webp"],
 		remotePatterns: [{ hostname: "cdn.sanity.io" }],
