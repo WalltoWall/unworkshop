248168626ef793d2981586f3a8fea7d980fc659e
Anthony Yarbrough <asyarb@hawaii.edu>
2024-04-30 11:56:05 -1000

    fix blanks in narrow

--- app/(site)/kickoff/[code]/exercises/[slug]/FormsExercise/NarrowField.tsx
+++ app/(site)/kickoff/[code]/exercises/[slug]/FormsExercise/NarrowField.tsx
@@ -13,15 +13,19 @@ type Props = FieldProps<{
 }>
 
 export const NarrowField = ({ source, answer, actions, ...props }: Props) => {
-	if (source.type !== "List")
+	if (source.type !== "List") {
 		throw new Error(
 			"Narrow fields only support List field answers as a source.",
 		)
-	if (answer && answer.type !== "Narrow")
+	}
+
+	if (answer && answer.type !== "Narrow") {
 		throw new Error("Invalid answer data found.")
+	}
 
 	const { max = Infinity } = props.field
 	const rForm = React.useRef<React.ElementRef<"form">>(null)
+	const responses = source.groups.at(0)?.responses.filter(Boolean)
 
 	const submitForm = () => {
 		if (!rForm.current || props.readOnly) return
@@ -55,39 +59,37 @@ export const NarrowField = ({ source, answer, actions, ...props }: Props) => {
 	return (
 		<form ref={rForm}>
 			<ul className="flex flex-col gap-2">
-				{source.groups.at(0)?.responses.map((response, idx) => {
-					return (
-						<li key={response}>
-							<label className="group flex cursor-pointer select-none items-center gap-2 rounded-lg border border-gray-50 bg-gray-90 py-1.5 pl-[5px] pr-3 has-[:checked]:border-black has-[:checked]:outline has-[:checked]:outline-1 has-[:checked]:outline-offset-0 has-[:checked]:outline-black">
-								<div className="flex h-6 w-6 items-center justify-center rounded-[5px] border border-gray-50 bg-white group-has-[:checked]:border-black group-has-[:checked]:bg-black">
-									<Text
-										style="heading"
-										size={18}
-										className="uppercase text-gray-50 group-has-[:checked]:text-white"
-									>
-										{idx + 1}
-									</Text>
-								</div>
-
-								<Text className="text-gray-50" size={16}>
-									{response}
+				{responses?.map((response, idx) => (
+					<li key={response}>
+						<label className="group flex cursor-pointer select-none items-center gap-2 rounded-lg border border-gray-50 bg-gray-90 py-1.5 pl-[5px] pr-3 has-[:checked]:border-black has-[:checked]:outline has-[:checked]:outline-1 has-[:checked]:outline-offset-0 has-[:checked]:outline-black">
+							<div className="flex h-6 w-6 items-center justify-center rounded-[5px] border border-gray-50 bg-white group-has-[:checked]:border-black group-has-[:checked]:bg-black">
+								<Text
+									style="heading"
+									size={18}
+									className="uppercase text-gray-50 group-has-[:checked]:text-white"
+								>
+									{idx + 1}
 								</Text>
+							</div>
 
-								<input
-									type="checkbox"
-									name={INPUT_NAME}
-									value={response}
-									className="appearance-none outline-none"
-									onClick={handleChange}
-									checked={answer?.responses.includes(response)}
-									readOnly={props.readOnly}
-								/>
+							<Text className="text-gray-50" size={16}>
+								{response}
+							</Text>
 
-								<CheckIcon className="ml-auto w-4 opacity-0 group-has-[:checked]:opacity-100" />
-							</label>
-						</li>
-					)
-				})}
+							<input
+								type="checkbox"
+								name={INPUT_NAME}
+								value={response}
+								className="appearance-none outline-none"
+								onClick={handleChange}
+								checked={answer?.responses.includes(response)}
+								readOnly={props.readOnly}
+							/>
+
+							<CheckIcon className="ml-auto w-4 opacity-0 group-has-[:checked]:opacity-100" />
+						</label>
+					</li>
+				))}
 			</ul>
 		</form>
 	)
