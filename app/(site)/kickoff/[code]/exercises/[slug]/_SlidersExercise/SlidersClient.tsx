6bd54e3a43e7f039d9374abb16894ad38f8a57f0
Anthony Yarbrough <asyarb@hawaii.edu>
2024-11-15 14:32:24 -1000

    fix contributor bug for slider

--- app/(site)/kickoff/[code]/exercises/[slug]/_SlidersExercise/SlidersClient.tsx
+++ app/(site)/kickoff/[code]/exercises/[slug]/_SlidersExercise/SlidersClient.tsx
@@ -47,7 +47,7 @@ export const SlidersClient = ({ exercise, participant, groupSlug }: Props) => {
 					item={slider}
 					answer={answer}
 					actions={actions}
-					readOnly={role !== "captain"}
+					readOnly={role ? role !== "captain" : false}
 				/>
 			)}
 
