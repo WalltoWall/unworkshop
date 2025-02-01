957f72379705171ce55d9196275da72d8fb65095
Anthony Yarbrough <asyarb@hawaii.edu>
2025-01-31 23:27:03 -1000

    hooray

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
 			"react/no-unescaped-entities": "off",
 			"@typescript-eslint/no-explicit-any": "off",
 			"@typescript-eslint/ban-ts-comment": "off",
+			"@typescript-eslint/no-namespace": "off",
 			"@typescript-eslint/no-unused-vars": [
 				"error",
 				{
