96449bc52ab4b107eaaf45178baf856087c02557
Anthony Yarbrough <asyarb@hawaii.edu>
2025-04-24 23:49:37 -1000

    router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom/client"
 import "./index.css"
+import ReactDOM from "react-dom/client"
+import { router } from "./router"
+import { RouterProvider } from "@tanstack/react-router"
 
 const root = document.getElementById("root")
 if (root) {
-	ReactDOM.createRoot(root).render(<div>Hello World</div>)
+	ReactDOM.createRoot(root).render(<RouterProvider router={router} />)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
 import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
+import { TanStackRouterVite as tanStackRouter } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), react(), cloudflare()],
+	plugins: [
+		tsconfigPaths(),
+		tailwindcss(),
+		react(),
+		tanStackRouter(),
+		cloudflare(),
+	],
 })
