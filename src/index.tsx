153aae76b4546608c60a21baea3d9e8b042f1ce7
Anthony Yarbrough <asyarb@hawaii.edu>
2025-04-24 23:42:41 -1000

    begin move to csr

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client"
+import "./index.css"
+
+const root = document.getElementById("root")
+if (root) {
+	ReactDOM.createRoot(root).render(<div>Hello World</div>)
+}
